d321668fd1cf242028ab5dcfc6fa512577d28c84
cyb3rjerry <cedric@cedricbrisson.ca>
2023-12-30 22:49:20 -0500

    named nodes

--- images.tf
+++ images.tf
@@ -1,7 +1,7 @@
 resource "proxmox_virtual_environment_file" "ubuntu-cloud-server-23" {
   content_type = "iso"
   datastore_id = "local"
-  node_name    = "pve"
+  node_name    = local.nodes.pve.name
 
   source_file {
     path = "https://cloud-images.ubuntu.com/lunar/current/lunar-server-cloudimg-amd64.img"
@@ -11,7 +11,7 @@ resource "proxmox_virtual_environment_file" "ubuntu-cloud-server-23" {
 resource "proxmox_virtual_environment_file" "windows-10-22H2" {
   content_type = "iso"
   datastore_id = "local"
-  node_name    = "pve"
+  node_name    = local.nodes.pve.name
 
   source_file {
     path = "https://software.download.prss.microsoft.com/dbazure/Win10_22H2_English_x64v1.iso?t=fbfea52d-5747-4db2-9fba-1a4bc2ad2cbb&e=1704076788&h=ca8a1fc4e187748e27904776606d8523e45351d4d7b0d734d76704a81fc01232"
@@ -21,5 +21,5 @@ resource "proxmox_virtual_environment_file" "windows-10-22H2" {
 resource "proxmox_virtual_environment_file" "virtio-drivers" {
   content_type = "iso"
   datastore_id = "local"
-  node_name    = "pve"
+  node_name    = local.nodes.pve.name
 }
--- locals.tf
+++ locals.tf
@@ -1,3 +1,21 @@
 locals {
   general_pool = "GenPurpose-Ceph-Pool"
+
+  nodes = {
+    pve = {
+      name   = "pve"
+      cpus   = 12
+      memory = 64
+    }
+    pve2 = {
+      name   = "pve2"
+      cpus   = 12
+      memory = 32
+    }
+    pve3 = {
+      name   = "pve3"
+      cpus   = 4
+      memory = 32
+    }
+  }
 }
