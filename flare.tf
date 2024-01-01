713d9e7d4a251ed361dfd219983c910c4d56c25c
cyb3rjerry <cedric@cedricbrisson.ca>
2024-01-01 14:24:47 -0500

    Tweaked the vm a bit to improve performance

--- flare.tf
+++ flare.tf
@@ -3,6 +3,12 @@ resource "proxmox_virtual_environment_vm" "testing" {
   name      = "flare"
   tags      = ["terraform", "windows", "malware"]
 
+  started = false // this way TF doesn't hang waiting on windows to boot
+
+  agent {
+    enabled = true
+  }
+
   disk {
     datastore_id = local.general_pool
     interface    = "scsi0"
@@ -10,7 +16,7 @@ resource "proxmox_virtual_environment_vm" "testing" {
   }
 
   memory {
-    dedicated = 16000
+    dedicated = 32000
   }
 
   /* Two cdroms currently isn't supported so we have to load the virtio-drivers
@@ -24,13 +30,25 @@ resource "proxmox_virtual_environment_vm" "testing" {
   */
 
   cdrom {
-    enabled   = true
+    enabled   = false // set to true on initial install
     file_id   = proxmox_virtual_environment_file.windows-10-22H2.id
     interface = "ide0"
   }
 
   cpu {
     cores = 8
+    type  = "host" // https://forum.proxmox.com/threads/my-windows-vm-is-very-slow.119271/
+    flags = ["+aes"]
   }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  operating_system {
+    type = "win10"
+  }
+
+  machine = "pc"
 }
 
